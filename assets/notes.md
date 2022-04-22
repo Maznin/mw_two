3defe8f2b6e992482a3d0a1845c49beb8d58eacb
Marko Mladenovic <marko.mladenovic988@gmail.com>
2022-04-22 12:40:27 +0200

    initial commit

---
+++ assets/notes.md
@@ -0,0 +1,9 @@
+Sections
+-Header
+    -- multijezicnost mozda
+-Hero
+-About
+-Why Us?
+-Contact section with CTA
+-Planning
+-Footer
---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo('charset'); ?>" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+
+<header class="header">
+	<div class="header-top"></div>
+	<div class="header-bottom">
+		<div class="header-bottom-col">
+			<h1>
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+					<?php bloginfo( 'name' ); ?>
+				</a>
+			</h1>
+		</div>
+		<div class="header-bottom-col">
+			<h1>
+				Col2
+			</h1>
+		</div>
+		<div class="header-bottom-col">
+			<h1>
+				Col3
+			</h1>
+		</div>
+	</div>
+</header>
+
+<main id="primary" class="site-main" role="main">
